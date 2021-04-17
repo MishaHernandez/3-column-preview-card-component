717af4c137acc1edf390056688a247e35dee9701
Mijail Hernandez <mijailhernandez@gmail.com>
2021-04-16 21:35:21 -0500

    customized tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightOrange: 'hsl(31, 77%, 52%)',
+        darkCyan: 'hsl(184, 100%, 22%)',
+        veryDarkCyan: 'hsl(179, 100%, 13%)',
+        paragraph: 'hsla(0, 0%, 100%, 0.75)',
+        veryLightGray: 'hsl(0, 0%, 95%)',
+      },
+      fontFamily: {
+        bigShoulders: ['Big Shoulders'],
+        lexendDeca: ['Lexend Deca'],
+      }
+    },
   },
   variants: {
     extend: {},
